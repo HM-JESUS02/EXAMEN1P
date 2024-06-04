088baad5a80ef98669d100ea580a483c5c2ae1cb
Jesus Hipolito <jesushipolito467@gmail.com>
2024-06-04 12:07:27 -0600

    tercer commitc

---
+++ src/app/logging.service.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+//import { LoggingService } from './services/logging.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  //constructor(private loggingService: LoggingService) { }
+
+  ngOnInit(): void {
+  }
+
+  logHolaMundo() {
+    //this.loggingService.logHolaMundo();
+  }
+}
